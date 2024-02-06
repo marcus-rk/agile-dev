7caab813a1f30c6e7a01f990483a19ffa0fc9bdd
marcus_rk <makj0005@stud.kea.dk>
2024-02-06 10:23:30 +0100

    Added comments to main.js

---
+++ 02_agile/node_server/main.js
@@ -0,0 +1,16 @@
+// Simple js hello world
+console.log("Hello World from main.js!");
+
+while (true) {
+    throw new Error("This is an error");
+}
+
+// Start application with pm2 (a server that runs code)
+//  $ `pm2 start <file>.js`
+//  $ pm2 list (show servers)
+//  $ pm2 stop <app_name_or_id> (stop server)
+//  $ pm2 delete <app_name_or_id> (delete server)
+
+// Parse JSON with jq
+//  $ brew install jq
+//  $ echo '{"hello":123}' | jq
