9d5e7af869448376c4497f5e6f73969dded5eea2
marcus_rk <makj0005@stud.kea.dk>
2024-02-07 10:47:09 +0100

    Updated node_server folder

--- 02_agile/node_server/main.js
+++ 02_agile/node_server/main.js
@@ -8,9 +8,12 @@ while (true) {
 // Start application with pm2 (a server that runs code)
 //  $ `pm2 start <file>.js`
 //  $ pm2 list (show servers)
-//  $ pm2 stop <app_name_or_id> (stop server)
-//  $ pm2 delete <app_name_or_id> (delete server)
+//  $ pm2 stop <app_name_or_id>     (stop server)
+//  $ pm2 delete <app_name_or_id>   (delete server)
 
 // Parse JSON with jq
 //  $ brew install jq
 //  $ echo '{"hello":123}' | jq
+
+// Github CLI (way of interacting with github through the terminal)
+//  $ brew install gh 
